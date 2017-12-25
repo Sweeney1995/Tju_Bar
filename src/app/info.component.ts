d6932d1ec6ad4fd489072a013d2414930691287e
Sweeney1995 <1296872186@qq.com>
2017-12-26 07:55:14 +0800

    sfy

--- src/app/info.component.ts
+++ src/app/info.component.ts
@@ -24,13 +24,20 @@ export class InfoComponent implements OnInit {
   title:string;
   heroes:Tie[];
   userPosts:Po[];
+  flag:boolean;
+  userId:number;
   constructor(private m:MethodService, private acroute:ActivatedRoute){}
-  go() {
-    this.m.goAddress('/perinfog','sss');
+  isShow(id:number) {
+    if(+sessionStorage.getItem('id')!==id) {
+      this.flag=false;
+    } else { this.flag=true; }
   }
   ngOnInit() {
     this.heroes=HEROES;
+    this.flag=true;
+    this.userId=(+sessionStorage.getItem('id'));
     this.title=sessionStorage.getItem('userName');
-    this.m.getUserPosts(+sessionStorage.getItem('id')).then(datas => this.userPosts =datas);
+    //this.m.getUserPosts(+sessionStorage.getItem('id')).then(datas => { this.userPosts =datas; alert(this.userPosts);});
+    this.m.findAllPosting().then(datas => this.userPosts=datas['postthingList']);
   }
 }
